28efa3ca556a20f689f13ac670581406c595be9c
venugi-perera <118472901+venugi-perera@users.noreply.github.com>
2024-05-26 18:03:22 +0530

    main py changed

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
-import numpy as np
-import pickle
-import os 
-from pydantic import BaseModel
-
 # from routes.route import router
 
 app = FastAPI()
-
-# model_path = 'model'
-
-# class PredictCaloriesItem(BaseModel):
-#     RPC_RATE: int 
-#     KEPT_RATE: int
-#     MONTH_END_DPD: int
-
-# with open(os.path.join(model_path, 'model.pkl'), 'rb') as f:
-#     model = pickle.load(f)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# @app.post('/predict')
+# @router.post('/predict')
 # async def predict_calories(item: PredictCaloriesItem):
 #     df = pd.DataFrame([item.dict()])
 #     preds = model.predict(df)
 #     rounded_preds = np.round(preds)
 #     return {'prediction': int(rounded_preds)}
-
 # origins = [
 #     "http://localhost:3000",
 #     "http://localhost:8000",
