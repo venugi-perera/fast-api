67c0f739013b89b4d3d33b52eac157bd09f5760d
venugi-perera <118472901+venugi-perera@users.noreply.github.com>
2024-05-26 17:50:26 +0530

    main.py changed

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from routes.route import router
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "https://vertexfinance.vercel.app",
-    "https://webrouter.vercel.app"
-]
+# origins = [
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+#     "https://vertexfinance.vercel.app",
+#     "https://webrouter.vercel.app"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
