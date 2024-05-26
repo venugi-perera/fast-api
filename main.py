e37730455da8fc60dbd732af9d41f0a24ad8c16b
venugi-perera <118472901+venugi-perera@users.noreply.github.com>
2024-05-26 18:31:16 +0530

    numpy package deleted

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 # from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 # import pickle
 # import os
 # from pydantic import BaseModel
